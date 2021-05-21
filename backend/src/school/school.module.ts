d83d976561fb1ace73ec7c2d689061e53dc34989
paul radic <paul.radic@epita.fr>
2021-05-22 00:16:25 +0200

    [SCHOOL] school typeorm import

--- backend/src/school/school.module.ts
+++ backend/src/school/school.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { SchoolController } from './school.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { School } from './entities/school.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([School])],
   controllers: [SchoolController],
   providers: [SchoolService]
 })
