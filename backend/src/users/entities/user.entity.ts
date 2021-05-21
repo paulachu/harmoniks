bbd53848630cd9510594a96269d6ce1800d8fd26
Samy Nalbandian <samy.nalbandian@epita.fr>
2021-05-22 00:09:36 +0200

    [USER] add school in users

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,6 +1,45 @@
-import { Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import * as crypto from 'crypto';
+import { School } from '../../school/entities/school.entity';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  full_name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @ManyToOne(type => School, school => school.users)
+  school_id: number;
+
+  @Column({ nullable: true })
+  debt: number;
+
+  @Column({ nullable: true })
+  skills: string;
+
+  @Column({ nullable: true })
+  discord_id: string;
+
+  @Column({ nullable: true })
+  linkedin_link: string;
+
+  @Column({ nullable: true })
+  hopper_link: string;
+
+  @BeforeInsert()
+  hashPassword() {
+    this.password = crypto.createHmac('sha256', this.password).digest('hex');
+  }
 
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  private readonly logger = new Logger(UsersService.name);
+
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    return this.usersRepository.save(createUserDto).catch(err =>
+    {
+      throw new HttpException('Invalid data', HttpStatus.BAD_REQUEST);
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find().catch(err => {
+      this.logger.error(err);
+      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): Promise<User> {
+    return this.usersRepository.findOne(id).catch(err => {
+      this.logger.error(err);
+      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, updateUserDto: UpdateUserDto): Promise<UpdateResult> {
+    return this.usersRepository.update(id, updateUserDto).catch(err => {
+      this.logger.error(err);
+      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: number): Promise<DeleteResult> {
+    return this.usersRepository.delete(id).catch(err => {
+      this.logger.error(err);
+      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    });
   }
 }
