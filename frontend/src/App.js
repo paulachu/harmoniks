e5ce8fc995d9e81b46388ad2524fd530e340d7ff
Akram Zabat <akram.zabat@epita.fr>
2021-05-22 14:19:35 +0200

    [SIGN]PAges done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import SignPage from './pages/SignPage';
+import { Provider } from "mobx-react";
+import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navigation from "./components/Navigation";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import user from "./stores/User";
+import Presentation from "./pages/Presentation";
 
 function App() {
-  return (
-    <div className="App">
-        <SignPage/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Provider user={user}>
+                    <Navigation />
+                    <AnimatePresence>
+                        <Switch>
+                            <Route path="/" exact component={Presentation}/>
+                            <Route path="/signin" exact component={SignIn} />
+                            <Route path="/signup" exact component={SignUp} />
+                            <Redirect to="/" />
+                        </Switch>
+                    </AnimatePresence>
+                </Provider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/pages/SignPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SignPage = () => {
-
-};
-
-export default SignPage;
