d8fb652a0172559c0999c006b6bfef1d201555f4
Akram Zabat <akram.zabat@epita.fr>
2021-05-23 06:32:37 +0200

    [ALL ROUTES SET UP]

--- frontend/src/components/AskRequest.js
+++ frontend/src/components/AskRequest.js
@@ -1,16 +1,36 @@
+import { inject, observer } from 'mobx-react';
 import React, { useState } from 'react';
+import compose from 'recompose/compose';
 import InputTags from './InputTags';
 
-const AskRequest = () => {
+const AskRequest = ({user, handleNewRequest}) => {
+    const [Title, setTitle] = useState("");
+    const [Description, setDescription] = useState("");
     const [tags, setTags] = useState([]);
+
+    const handleSubmit = () => {
+        const tag_res = [];
+        tags.forEach((tag) => tag_res.push(tag.tags));
+        const data = {
+            "title" : Title,
+            "description": Description,
+            "skills": tag_res
+        };
+        user.postRequest(data).then(res => {
+            handleNewRequest(res)
+        });
+    }
+
     return (
-        <div className="container-request">   
+        <div className="container-request">
             <div className="wrapper-1">
-                <div className="content-name"> 
+                <div className="content-name">
                     <h2>Need help ?</h2>
                 </div>
                 <div className="content-title">
                     <input
+                            value={Title}
+                            onChange={(e) => setTitle(e.target.value)}
                             placeholder="Enter your title"
                             type="text"
                             className="box-container"
@@ -30,15 +50,17 @@ const AskRequest = () => {
                 <div className="content-desc">
                     <h4 className="content-desc-1"> Description :</h4>
                     <textarea placeholder="Detailes your request"
-                        rows="3" cols="3" maxlength="500" className="text-container">
+                        rows="3" cols="3" maxlength="500" className="text-container"
+                        value={Description}
+                        onChange={(e) => setDescription(e.target.value)}>
                     </textarea>
                 </div>
                 <div className="content-add">
-                    <button className="button-submit">Send</button>
+                    <button className="button-submit" onClick={handleSubmit}>Send</button>
                 </div>
             </div>
         </div>
     );
 };
 
-export default AskRequest;
+export default compose(inject("user"), observer) (AskRequest);
--- frontend/src/pages/RequestsPage.js
+++ frontend/src/pages/RequestsPage.js
@@ -1,22 +1,53 @@
-import React, { useState } from "react";
-import InputTags from "../components/InputTags";
+import React, { useEffect, useState } from "react";
 import AskRequest from "../components/AskRequest";
 import ResumeRequest from "../components/ResumeRequest";
 import { motion } from "framer-motion";
+import compose from "recompose/compose";
+import { inject, observer } from "mobx-react";
 
-const RequestsPage = () => {
-    const [askRequest, setaskRequest] = useState([]);
+const RequestsPage = ({ user }) => {
+    const [loaded, setLoaded] = useState(false);
+    const [requests, setRequests] = useState([]);
+    const [myRequest, setMyRequest] = useState(null);
+
+    useEffect(() => {
+        if (!loaded) {
+            // Get All request in resquests
+            user.getAllRequests().then((res) => {
+                // Get MyRequest
+                user.getMyRequest().then((res_bis) => {
+                    const my_elt = res_bis.find((e) => e.status === 0); 
+                    setMyRequest(my_elt);
+                    setRequests(res.filter(e => JSON.stringify(e) !== JSON.stringify(my_elt)));
+                });
+            });
+            // Set Loaded to true
+            setLoaded(true);
+        }
+    }, [loaded]);
+
+    const handleNewRequest = (request) => {
+        setMyRequest(request);
+    };
+
+    const handleDelete = () => {
+        user.deleteRequest();
+        setMyRequest(null);
+    }
+    
     return (
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             className="requestsPage">
-            <AskRequest />
-            <ResumeRequest />
-            <ResumeRequest />
+            {!myRequest && <AskRequest handleNewRequest={handleNewRequest}/>}
+            {myRequest && <ResumeRequest request={myRequest} isMine={true} handleDelete={handleDelete}/>}
+            {requests.map((request) => {
+                return <ResumeRequest request={request} />;
+            })}
         </motion.div>
     );
 };
 
-export default RequestsPage;
+export default compose(inject("user"), observer)(RequestsPage);
