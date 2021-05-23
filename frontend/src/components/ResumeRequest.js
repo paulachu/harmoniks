60362f66916bd158342dbc6b0c1e1639a60b2368
EvanHays <evan.hays@epita.fr>
2021-05-23 03:10:51 +0200

    [FRONT] Resume request

--- frontend/src/components/ResumeRequest.js
+++ frontend/src/components/ResumeRequest.js
@@ -2,25 +2,41 @@ import React, { useState } from 'react';
 
 
 const ResumeRequest = () => {
+    var statusValue = 1;
+    const red = {backgroundColor : "#C9846F"};
+    const green = {backgroundColor : "#8DB0BA"};
     return (
         <div className="container-resume-request">
 
             <div className="wrapper-resume-request">
                 <div className="resume">
-                    <h2>this is my title</h2>
+                    <h2 className="title">this is my title</h2>
 
-                    <h4> Description :</h4>
+                    <h4 className="title"> Description :</h4>
                     <label className="text-container"> blabdwai dwua dbuiaw bduiaw dwa dwa dad wa dwa dwa dwadwa
                     </label>
                     
                 </div> 
+                
+                <div className="status">
+                    <label style={statusValue < 0 ? red : green} className="content-status">{statusValue}</label>
+                </div>
 
                 <div className="skils">
-                    <h2>Skils required :</h2>
+                    <h2 className="title">Skils required :</h2>
                     <div className="wrapper-skils">
                         <label className="content-skils">Reactjs</label>
                         <label className="content-skils">Nodejs</label>
                     </div>
+                </div>
+
+                <div className="join-discord">
+                    <img src="/logos/discordLogo.png" className="logo-image"></img>
+                </div>
+
+                <div className="user">
+                    <img src="/logos/userImg.jpeg" className="user-image"></img>
+                    <label className="textbot">Joe Blein</label>
                 </div>
             </div>
         </div>
